ff377df501ab6a1f10c12aeb16b5ae575add1a71
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-09-20 14:13:43 -0700

    neccessary files and gitignore

--- models/place.py
+++ models/place.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
 from sqlalchemy.orm import relationship
 import os
