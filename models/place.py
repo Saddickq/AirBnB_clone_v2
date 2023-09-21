6c240f655fa0c240c8a3c30e21cf21605f30a990
Ardiy <you@example.com>
2023-09-21 17:03:53 +0000

    place amenities and setter logic

--- models/place.py
+++ models/place.py
@@ -1,15 +1,16 @@
-#!/usr/bin/python3
-""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from sqlalchemy import Table, Column, String, ForeignKey, Integer, Float, PrimaryKeyConstraint
 from sqlalchemy.orm import relationship
-import os
+from os import getenv
+from models.review import Review
+from models.amenity import Amenity
 
 
 class Place(BaseModel, Base):
     """ Class for Place Module """
     __tablename__ = "places"
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         name = Column(String(128), nullable=False)
@@ -20,8 +21,26 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, default=0, nullable=False)
         latitude = Column(Float)
         longitude = Column(Float)
+
         reviews = relationship('Review', backref='place',
                                cascade='all, delete, delete-orphan')
+
+        place_amenity = Table(
+            'place_amenity',
+            Base.metadata,
+            Column('place_id', String(60), ForeignKey(
+                'places.id'), primary_key=True, nullable=False),
+            Column('amenity_id', String(60), ForeignKey(
+                'amenities.id'), primary_key=True, nullable=False),
+            PrimaryKeyConstraint('place_id', 'amenity_id')
+        )
+
+        amenities = relationship(
+            'Amenity',
+            secondary=place_amenity,
+            viewonly=False
+        )
+
     else:
         city_id = ""
         user_id = ""
@@ -41,12 +60,38 @@ class Place(BaseModel, Base):
             id matches place_id"""
             from models import storage
             matched_results = []
-            """get all the review objects i teh review file"""
+            """get all the review objects in the review file"""
             reviews = storage.all(Review)
             for review in reviews.values():
-                """if review models place id matches the id of
+                """if review model's place id matches the id of
                 the current place id return those particular
                 objects or instances"""
                 if review.place_id == self.id:
                     matched_results.append(review)
             return matched_results
+        amenity_ids = []
+
+        @property
+        def amenities(self):
+            """
+            Getter attribute that returns a list of Amenity instances
+            based on the attribute amenity_ids that contains all Amenity.id
+            linked to the Place.
+            """
+            from models import storage
+            amenity_instances = []
+            for amenity_id in self.amenity_ids:
+                amenity = storage.get(Amenity, amenity_id)
+                if amenity:
+                    amenity_instances.append(amenity)
+            return amenity_instances
+
+        @amenities.setter
+        def amenities(self, obj):
+            """
+            Setter attribute that handles the append method for adding an
+            Amenity.id to the attribute amenity_ids. This method should
+            accept only Amenity object, otherwise, do nothing.
+            """
+            if isinstance(obj, Amenity):
+                self.amenity_ids.append(obj.id)
