2941b69b795557e92b9b72908e0ee0b666d0585d
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-10-05 17:09:50 +0000

    deploy first webpages

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# a Bash script that sets up your web servers for the deployment of web_static
+sudo apt update -y
+sudo apt install nginx -y
+
+#sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo chown -R "$USER":"$USER" /data
+touch /data/web_static/releases/test/index.html
+cd /data/web_static/releases/test/
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+sed -i '/error_page/a\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sudo service nginx restart
