e024bfe4efad7c7c5eacae4451e3675902580968
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-10-10 11:23:24 +0000

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""
-Fabric script to distribute an archive to web servers
-"""
-
-from fabric.api import run, put, env
+from fabric.api import run, env, put
 from os.path import exists
+"""
+a Fabric script (based on the file 1-pack_web_static.py) that distributes
+an archive to your web servers, using the function do_deploy:
+"""
 
 env.hosts = ['100.26.164.241', '34.229.49.155']
 env.user = 'ubuntu'
@@ -12,29 +12,24 @@ env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to web servers
-    """
+    """ Deploy archive to web-servers """
     if not exists(archive_path):
         return False
 
     try:
-        put(archive_path, "/tmp/")
+        put(archive_path, '/tmp/')
+        archive_file = archive_path.split('/')[-1]
+        archive_folder = archive_file.split('.')[0]
+        release_folder = '/data/web_static/releases/{}'.format(archive_folder)
 
-        archive_filename = archive_path.split("/")[-1]
-        folder_name = archive_filename.split(".")[0]
-        release_path = "/data/web_static/releases/{}/".format(folder_name)
-        run("mkdir -p {}".format(release_path))
-        run("tar -xzf /tmp/{} -C {}".format(archive_filename, release_path))
-
-        run("rm /tmp/{}".format(archive_filename))
-
-        run("rm -f /data/web_static/current")
-
-        # Create a new symbolic link
-        run("ln -s {} /data/web_static/current".format(release_path))
-
-        return True
-
+        run('mkdir -p {}'.format(release_folder))
+        run('tar -xzf /tmp/{} -C {}'.format(archive_file, release_folder))
+        run('rm /tmp/{}'.format(archive_file))
+        run('mv {}/web_static/* {}'.format(release_folder, release_folder))
+        run('rm -rf /data/web_static/current')
+        run('rm -rf {}/web_static'.format(release_folder))
+        run('ln -s {}/ /data/web_static/current'.format(release_folder))
     except Exception:
         return False
+    else:
+        return True
